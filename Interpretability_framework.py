b07017ffaf85ff17f0f265c9931d1008ca526100
jiahzengW <102720045+jiahzengW@users.noreply.github.com>
2024-10-26 14:14:50 +0800

    Update and rename Untitled.py to Interpretability_framework.py

--- Interpretability_framework.py
+++ Interpretability_framework.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import shap
 import numpy as np
 import pandas as pd
@@ -19,8 +13,6 @@ X, y = shap.datasets.adult()
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Model Training
-
-
 # Global feature importance analysis using SHAP
 explainer_shap = shap.TreeExplainer(model)
 shap_values = explainer_shap.shap_values(X_train)
