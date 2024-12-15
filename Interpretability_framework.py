39d0f7ed578b64f4855dacb8863c0b533afe9891
jiahzengW <102720045+jiahzengW@users.noreply.github.com>
2024-12-15 19:14:26 +0800

    Update Interpretability_framework.py

--- Interpretability_framework.py
+++ Interpretability_framework.py
@@ -8,7 +8,7 @@ from alibi.explainers import IntegratedGradients
 from interpret import show
 import matplotlib.pyplot as plt
 
-# Load dataset and split into train/test sets
+# Load dataset and split into train/test set
 X, y = shap.datasets.adult()
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
